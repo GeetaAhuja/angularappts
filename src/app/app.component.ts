79e40371bf053c9a65f76cdfba0df150a591de4e
geetaahuja <geetu_ahuja@yahoo.com>
2018-10-30 15:00:52 -0500

    checkin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import * as MyModule from '../amd/mymodule.js';
+import * as MyNewAMDModule from '../amd/myNewAMDModule.js';
 import * as MyAMDModule from '../amd/myNewAMDModule.js';
+
 import {CommonService} from './common.service';
 
 @Component({
@@ -12,8 +14,11 @@ export class AppComponent {
 
   constructor(private cs:CommonService){
     console.log(MyModule.value); 
-    console.log(MyModule.foo(),'Foo');
-    console.log(MyAMDModule,'Foo1');
+    console.log(MyModule.foo(),'Foo My Module should return json {a:1, B:"test"} ');
+    console.log(MyNewAMDModule,'Foo1 My New AMD Module should return {color:black,unisize:test}');
+    console.log(MyAMDModule,'Foo1 My AMD Module should return {color:black,unisize:test}');
+
+
   }
   title = 'my-app';
 }
